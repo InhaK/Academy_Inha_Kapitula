de9c21b5ff3169e4c639bf54c7f598313a0083e8
Inha Kapitula <inha.kapitula@eleks.com>
2018-07-31 19:52:52 +0300

    Cannot finish the last task

---
+++ JS Practice 3/4.js
@@ -0,0 +1,55 @@
+/*let rectangle = {
+    width: 5,
+    height: 10,
+    getArea: function() {return this.width * this.height;}
+};
+console.log('Area: ${rectangle.getArea()}');*/
+
+/*function getArea(w,h){
+    let Area = w*h;
+    return Area;
+}
+
+getArea(3,5);
+
+console.log("Area is equal to" [ts].getArea());
+
+
+
+function Rectangle(height, width) {
+    this.height = height;
+    this.width = width;
+    this.calcArea = function() {
+        return this.height * this.width;
+    };
+  function calcPerimeter() {
+      return 2 * this.height + 2 * this.width;
+      }
+  }
+  
+  let rex = new Rectangle(7,3);
+  let area = rex.calcArea();
+  let perimeter = rex.calcPerimeter();
+
+
+
+  let rectangle = {
+      width:2,
+      height:3,
+      getArea: function() {return this.width*this.height}
+  };
+  console.log('Rectangle area is: ${rectangle["getArea()"]}');
+
+
+
+
+
+  let perimeterBox = function (length, width) {
+    return 2 * (length + width);
+};
+
+let lenht = prompt("Enter Lenght");
+let widht = prompt("Enter Wight");
+let perimeterBox = 2 * (lenght + widht);
+
+console.log("perimeterBox");*/
