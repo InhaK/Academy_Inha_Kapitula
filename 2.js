aaeae7a01e94c83c82c422e3dd8b6d610e8dcdeb
InhaK <41836418+InhaK@users.noreply.github.com>
2018-09-21 15:50:44 +0300

    Delete 2.js

--- 2.js
+++
@@ -1,9 +0,0 @@
-console.log(`1 step`);
-console.log(`2 step`);
-
-new Promise((resolve, reject) => setTimeout(() => {
-    console.log(`3 step`);
-    resolve();
-}, 2000)).then(() => {
-    console.log(`4 step`);
-});
