a0d855691c04f149cb6c07d58885949ad2b085ab
InhaK <41836418+InhaK@users.noreply.github.com>
2018-09-21 15:51:04 +0300

    Delete 3.js

--- 3.js
+++
@@ -1,9 +0,0 @@
-Promise.all([
-    new Promise((resolve, reject) => setTimeout(() => resolve(1), 2000)),
-    new Promise((resolve, reject) => setTimeout(() => resolve(2), 2000)),
-    new Promise((resolve, reject) => setTimeout(() => resolve(3), 2000))
-]).then((results) => {
-    console.log(results[0]);
-    console.log(results[1]);
-    console.log(results[2]);
-});
